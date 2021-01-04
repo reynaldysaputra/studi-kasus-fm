8ec4a1b160e5b0f5a7571a8188296258bb3e5ff8
Renaldy <renalfrontend@gmail.com>
2021-01-04 21:02:05 +0700

    First commit studi kasus framer motion, dan complete project scrollEffect_1

---
+++ src/01_scrollEffect_1/apps.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import style from './style.module.css';
+import {useMotionValue, motion, useTransform, useViewportScroll} from 'framer-motion';
+
+function AppsEffect_1() {
+   const {scrollYProgress} = useViewportScroll();
+   const scaleBox = useTransform(scrollYProgress, [0,1], [0.5,1.8]);
+   const marginTopStyle = useTransform(scrollYProgress, [0,1], ['100px','0px']);
+
+   return(
+      <Fragment>
+         <section>
+            <motion.div 
+               className={style.box} 
+               style={{scale:scaleBox, transition:.5}}
+            >
+               <motion.div 
+                  className={style.childBox}
+                  style={{marginTop:marginTopStyle}}
+               ></motion.div>
+            </motion.div>
+         </section>
+      </Fragment>
+   )
+}
+
+export default AppsEffect_1;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppsEffect_1 from './01_scrollEffect_1/apps';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppsEffect_1 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
